c9bfe28bdb6d0fc0e2b82a6f2c9356b5ebec1ba7
Abigail McIntosh <abimcintosh56@gmail.com>
2025-10-17 10:35:13 +0100

    mother method

--- pythia_test.py
+++ pythia_test.py
@@ -16,13 +16,14 @@ pythia.init()
 
 with h5py.File(output, 'w') as h5file:
     dtypes = np.dtype([
-        ('id', 'i4'),
-        ('state', 'S1'),
-        ('pdg_id', 'i4'),
+        ('id', 'i8'),
+        ('pdg_id_hadron', 'i4'),
+        ('pdg_id_quark'),
         ('px', 'f8'),
         ('py', 'f8'),
         ('pz', 'f8'),
-        ('e', 'f8')
+        ('e_hadron', 'f8'),
+        ('e_quark', 'f8'),
     ])
     dset = h5file.create_dataset(
         'particles', 
@@ -31,3 +32,32 @@ with h5py.File(output, 'w') as h5file:
         dtype=dtypes,
         chunks=True 
     )
+
+hadron_ids = [411, 421, 431, 4122, -411, -421, -431, -4122]
+
+event_id = 0
+total_rows = 0
+
+while event_id < no_events:
+    rows = []
+
+    for _ in range(min(chunk_size, no_events - event_id)):
+        if not pythia.next():
+            continue
+
+        quarks = []
+        hadrons = []
+
+        for p in pythia.event:
+            if abs(p.id()) == 4:
+                p.append(quarks)
+            elif p.id() in hadron_ids:
+                p.append(hadrons)
+
+        for q in quarks:
+            for h in hadrons:
+                mothers = h.motherList()
+                if q.index() in mothers:
+                    rows.append((event_id, h.id(), q.id(), h.px(), h.py(), h.pz(), h.e(), q.e()))
+
+    event_id += 1

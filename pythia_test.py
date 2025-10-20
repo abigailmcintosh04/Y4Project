968f46e4e735fa25e5b612bc16d0b36c7d6ffeaf
Abigail McIntosh <abimcintosh56@gmail.com>
2025-10-20 22:28:13 +0100

    full pythia generator

--- pythia_test.py
+++ pythia_test.py
@@ -3,45 +3,51 @@ import h5py
 import numpy as np
 
 
-no_events = 10000
-chunk_size = 100
+no_events = 100000
+chunk_size = 10000
 output = 'collisions.h5'
 
 pythia = pythia8mc.Pythia()
+
 pythia.readString('Beams:idA = 2212')
 pythia.readString('Beams:idB = 2212')
-pythia.readString('Beams:eCM = 13600')
-pythia.readString('HardQCD:all = on')
+pythia.readString('Beams:eCM = 13000.')
+# pythia.readString('HardQCD:all = on')
+pythia.readString("PartonLevel:ISR = on")
+pythia.readString("PartonLevel:FSR = on")
+pythia.readString("HadronLevel:Hadronize = on")
+pythia.readString("HardQCD:gg2ccbar = on")
+pythia.readString("HardQCD:qqbar2ccbar = on")
+
+pythia.readString("Next:numberShowInfo = 0")
+pythia.readString("Print:quiet = on")
+pythia.readString("Next:numberShowEvent = 0")
 pythia.init()
 
-with h5py.File(output, 'w') as h5file:
-    dtypes = np.dtype([
-        ('id', 'i8'),
+hadron_ids = [411, 421, 431, 4122, -411, -421, -431, -4122]
+quark_ids = [4, -4]
+
+dtype = np.dtype([
         ('pdg_id_hadron', 'i4'),
-        ('pdg_id_quark'),
-        ('px', 'f8'),
-        ('py', 'f8'),
-        ('pz', 'f8'),
         ('e_hadron', 'f8'),
+        ('pdg_id_quark', 'i4'),
         ('e_quark', 'f8'),
-    ])
+])
+
+with h5py.File(output, 'w') as h5file:
     dset = h5file.create_dataset(
         'particles', 
         shape=(0,),
         maxshape=(None,),
-        dtype=dtypes,
+        dtype=dtype,
         chunks=True 
     )
 
-hadron_ids = [411, 421, 431, 4122, -411, -421, -431, -4122]
+    event_id = 0
+    total_rows = 0
+    buffer = []
 
-event_id = 0
-total_rows = 0
-
-while event_id < no_events:
-    rows = []
-
-    for _ in range(min(chunk_size, no_events - event_id)):
+    while event_id < no_events:
         if not pythia.next():
             continue
 
@@ -49,15 +55,32 @@ while event_id < no_events:
         hadrons = []
 
         for p in pythia.event:
-            if abs(p.id()) == 4:
-                p.append(quarks)
+            if p.id() in quark_ids:
+                quarks.append(p)
             elif p.id() in hadron_ids:
-                p.append(hadrons)
+                hadrons.append(p)
 
-        for q in quarks:
+        charm_rows = []
+        for c in quarks:
             for h in hadrons:
-                mothers = h.motherList()
-                if q.index() in mothers:
-                    rows.append((event_id, h.id(), q.id(), h.px(), h.py(), h.pz(), h.e(), q.e()))
+                if c.index() in h.motherList():
+                    charm_rows.append((h.id(), h.e(), c.id(), c.e()))
 
-    event_id += 1
+        if charm_rows:
+            buffer.extend(charm_rows)
+            event_id += 1
+
+        if event_id % chunk_size == 0 and buffer:
+            arr = np.array(buffer, dtype=dtype)
+            dset.resize(total_rows + len(arr), axis=0)
+            dset[total_rows:total_rows + len(arr)] = arr
+            total_rows += len(arr)
+            buffer = []
+
+    if buffer:
+        arr = np.array(buffer, dtype=dtype)
+        dset.resize(total_rows + len(arr), axis=0)
+        dset[total_rows:total_rows + len(arr)] = arr
+        total_rows += len(arr)
+
+print('Events saved.')

1e25b9f68fe1b0e85b3c94739a0e9f68673a461e
Abigail McIntosh <abimcintosh56@gmail.com>
2026-02-16 15:54:59 +0000

    fixed event_analysis for help fixing d0 script

--- classification/event_analysis.py
+++ classification/event_analysis.py
@@ -1,20 +1,31 @@
 import fastjet
 
 from generator_utils import configure_pythia, single_event
+from d0_generator import is_signal_track
 
 pythia = configure_pythia()
 jet_def = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.4)
 
 if pythia.next():
-    constituents, h, best_jet = single_event(pythia.event, jet_def, consts=True)
+    constituents, h, best_jet = single_event(pythia.event, jet_def, ptmin=20.0, consts=True)
+    if not constituents:
+        print('No constituents found for this event.')
+        exit()
+
     print(f"\n--- Analysis for Hadron {h.name()} ({h.id()}) matched to Jet with pT={best_jet.pt():.2f} ---")
     for i, c in enumerate(constituents):
         p = pythia.event[c.user_index()]
         p_id = p.id()
 
+        signal = 'no'
+        if is_signal_track(p, h.index(), pythia.event):
+            signal = 'yes'
+            
         print(f'Particle index: {i}')
         print(f'Name: {p.name()} (ID: {p_id})')
         print(f'4-Momentum: ({p.px():.2f}, {p.py():.2f}, {p.pz():.2f}, {p.e():.2f}) GeV')
-        print(f'pT: {p.pT():.2f} GeV, Mass: {p.m():.2f} GeV')
-        print(f'Eta: {p.eta():.2f}, Phi: {p.phi():.2f}')
+        # print(f'pT: {p.pT():.2f} GeV, Mass: {p.m():.2f} GeV')
+        # print(f'Eta: {p.eta():.2f}, Phi: {p.phi():.2f}')
+        print(f'Signal: {signal}')
+        print('\n')
 
